fb2f396bf9b6d3462874dd8c3107f192c8b59cff
bpourian <b.pourian@hotmail.co.uk>
2018-10-24 15:47:30 +0100

    Create simple test to demo use of gson for a simple conversion

---
+++ src/main/java/com/jtjo/model/Alpha.java
@@ -0,0 +1,24 @@
+package com.jtjo.model;
+
+/**
+ *
+ * @author Benjamin Pourian (benjamin.pourian@gmail.com)
+ */
+public class Alpha {
+
+    private String name;
+    private int age;
+    private String favouriteColour;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFavouriteColour() {
+        return favouriteColour;
+    }
+}
